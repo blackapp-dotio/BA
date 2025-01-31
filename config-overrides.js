c7a5a9b851111596953b4068d93c95b10da0cc42
Emmanuel Achiri <achiriglobal@gmail.com>
2025-01-31 12:27:27 -0500

    Fix Webpack 5 missing crypto polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,18 @@
 const webpack = require("webpack");
 
 module.exports = function override(config) {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "util": require.resolve("util"),
-    "buffer": require.resolve("buffer"),
-    "process": require.resolve("process/browser"),
-  };
+  // Ensure plugins array exists
+  if (!config.plugins) {
+    config.plugins = [];
+  }
 
-  config.plugins = [
-    ...config.plugins,
+  // Add Webpack plugins
+  config.plugins.push(
     new webpack.ProvidePlugin({
       process: "process/browser",
       Buffer: ["buffer", "Buffer"],
-    }),
-  ];
+    })
+  );
 
   return config;
 };
