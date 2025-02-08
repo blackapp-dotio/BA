ad561ad5d9e13053bbd2ceaf4a1500d315fa0d1c
blackapp-dotio <support@blackapp.io>
2025-02-07 19:52:45 -0500

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,17 @@
 const webpack = require("webpack");
-const webpack = require("webpack");
 
 module.exports = function override(config) {
-  if (!config.resolve) {
-    config.resolve = {};
-  }
-  
   config.resolve.fallback = {
     ...config.resolve.fallback,
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
-    buffer: require.resolve("buffer/"),
+    buffer: require.resolve("buffer/")
   };
 
-  // Add Webpack plugins
   config.plugins.push(
     new webpack.ProvidePlugin({
-      process: "process/browser",
       Buffer: ["buffer", "Buffer"],
+      process: "process/browser"
     })
   );
 
